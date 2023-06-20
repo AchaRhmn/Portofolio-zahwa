4abb819aac4bf91498de842d8db9fed6e8eafa12
Acha <109843061+AchaRhmn@users.noreply.github.com>
2023-06-20 11:23:45 +0800

    Update Projects.jsx

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -15,23 +15,39 @@ const projectVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
+const projects = [
+  {
+    title: "Kwitansi",
+    desc: "A receipt made with PHP",
+    link: "http://kwitansi001.rf.gd/"
+  },
+  {
+    title: "OCHA",
+    desc: "A profile website for a tea shop made with CMS Wordpress",
+    link: ""
+  },
+  {
+    title: "SEO On Page",
+    desc: "An SEO project about drilled well",
+    link: "https://sumurbordewata.com/"
+  },
+  {
+    title: "First Project",
+    desc: "The first project that I made using HTML and CSS",
+    link: "https://firstproject-acharhmn.vercel.app/"
+  }
+];
 
-
-const Project = ({ title , desc}) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+const Project = ({ title, desc, link }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
         <p className="text-3xl font-playfair">{title}</p>
-        <p className="mt-7 text-xl">
-         {desc}
-        </p>
-        <a target="_blank"  className="hover:text-yellow transition duration-500 mt-[50px]">
-          
-          </a>
+        <p className="mt-7 text-xl">{desc}</p>
+        <a target="_blank" href={link} className="hover:text-yellow transition duration-500 mt-[50px]">Visit Site </a>
       </div>
       <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />
     </motion.div>
@@ -58,16 +74,13 @@ const Projects = () => {
             <span className="text-blue03">PRO</span>JECTS
           </p>
           <div className="flex justify-center mt-5 mb-5">
-            <LineGradient width="w-2/3"/>
+            <LineGradient width="w-2/3" />
           </div>
         </div>
-
-       
       </motion.div>
 
       {/* PROJECTS */}
       <div className="flex justify-center">
-      
         <motion.div
           className="sm:grid sm:grid-cols-3"
           variants={container}
@@ -75,28 +88,22 @@ const Projects = () => {
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
         >
-          
-          <div
-            className="flex justify-center text-center items-center p-10 bg-blue02
-              max-w-[400px] max-h-[400px] text-3xl font-playfair font-semibold"
-          >
-            Here are some projects that I made.
-          </div>
-          <Project title="Kwitansi" desc="A receipt made with PHP"/>
-         
-          <Project title="OCHA" desc="A profile website for tea shop made with CMS Wordpress" />
-
-          
-          <Project title="SEO On Page" desc="An SEO project about drilled well" />
-          <Project title="First Project" desc="The first project that I made using HTML and CSS" />
-          
-          <div
-            className="flex justify-center text-center items-center p-10 bg-blue
-              max-w-[400px] max-h-[400px] text-3xl font-playfair font-semibold"
-          >
-           And various other simple projects.
+          <div className="flex justify-center text-center items-center p-10 bg-blue02 max-w-[400px] max-h-[400px] text-3xl font-playfair font-semibold">
+            Here are some projects that I have made.
           </div>
           
+          {projects.map((project, index) => (
+            <Project
+              key={index}
+              title={project.title}
+              desc={project.desc}
+              link={project.link}
+            />
+          ))}
+          
+          <div className="flex justify-center text-center items-center p-10 bg-blue max-w-[400px] max-h-[400px] text-3xl font-playfair font-semibold">
+            And various other simple projects.
+          </div>
         </motion.div>
       </div>
     </section>
