d9a43c6b73d870d78f7b8f65676e70eef8be426a
Acha <109843061+AchaRhmn@users.noreply.github.com>
2023-06-21 11:46:03 +0800

    Update DotGroup.jsx

--- src/scenes/DotGroup.jsx
+++ src/scenes/DotGroup.jsx
@@ -1,42 +1,36 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
+const Dot = ({ selected, onClick, sectionId }) => {
+  const dotStyles = `relative bg-yellow before:absolute before:w-10 before:h-10 before:rounded-full
+    before:border-2 before:border-yellow before:left-[-50%] before:top-[-50%] mr-10`;
+
+  return (
+    <AnchorLink 
+      href={`#${sectionId}`} 
+      className={`${selected ? dotStyles : "bg-dark-grey"} w-5 h-5 rounded-full`}
+      onClick={onClick}
+    />
+  );
+};
+
 const DotGroup = ({ selectedPage, setSelectedPage }) => {
-  const selectedStyles = `relative bg-yellow before:absolute before:w-6 before:h-6 before:rounded-full
-    before:border-2 before:border-yellow before:left-[-50%] before:top-[-50%]`;
+  const dots = [
+    { id: "home", label: "Home" },
+    { id: "skills", label: "Skills" },
+    { id: "projects", label: "Projects" },
+    { id: "contact", label: "Contact" },
+  ];
 
   return (
     <div className="flex flex-col gap-6 fixed top-[60%] right-7">
-      <AnchorLink
-        href="#home"
-        className={`${
-          selectedPage === "home" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
-        onClick={() => setSelectedPage("home")}
-      />
-
-      <AnchorLink
-        href="#skills"
-        className={`${
-          selectedPage === "skills" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
-        onClick={() => setSelectedPage("skills")}
-      />
-
-      <AnchorLink
-        href="#projects"
-        className={`${
-          selectedPage === "projects" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
-        onClick={() => setSelectedPage("projects")}
-      />
-
-      <AnchorLink
-        href="#contact"
-        className={`${
-          selectedPage === "contact" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
-        onClick={() => setSelectedPage("contact")}
-      />
+      {dots.map((dot) => (
+        <Dot
+          key={dot.id}
+          selected={selectedPage === dot.id}
+          onClick={() => setSelectedPage(dot.id)}
+          sectionId={dot.id} 
+        />
+      ))}
     </div>
   );
 };
