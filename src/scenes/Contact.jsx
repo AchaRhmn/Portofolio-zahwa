8d5fdba8cd9ce5e490f41c2dfa59bc07c0013d7a
Acha <109843061+AchaRhmn@users.noreply.github.com>
2023-06-22 08:41:58 +0800

    Update Contact.jsx

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -19,21 +19,7 @@ const Contact = () => {
 
   return (
     <section id="contact" className="contact py-48">
-      {/* HEADINGS */}
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        transition={{ duration: 0.5 }}
-        variants={{
-          hidden: { opacity: 0, x: 50 },
-          visible: { opacity: 1, x: 0 },
-        }}
-        className="flex justify-center w-full "
-      >
-        
-      </motion.div>
-
+     
       {/* FORM & IMAGE */}
       <div className="md:flex md:justify-between gap-16 mt-5">
         <motion.div
