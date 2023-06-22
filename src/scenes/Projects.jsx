e356c39b3037e2d4f30dbceff3b3cb7179b81a65
Acha <109843061+AchaRhmn@users.noreply.github.com>
2023-06-22 14:02:15 +0800

    Update Projects.jsx

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -17,37 +17,41 @@ const projectVariant = {
 
 const projects = [
   {
+    id: "kwitansi",
     title: "Kwitansi",
     desc: "A receipt made with PHP",
     link: "http://kwitansi001.rf.gd/"
   },
   {
+    id: "ocha",
     title: "OCHA",
     desc: "A profile website for a tea shop made with CMS Wordpress",
-    link: ""
+    
   },
   {
+    id: "seo",
     title: "SEO On Page",
     desc: "An SEO project about drilled well",
     link: "https://sumurbordewata.com/"
   },
   {
+    id: "firstpro",
     title: "First Project",
     desc: "The first project that I made using HTML and CSS",
     link: "https://firstproject-acharhmn.vercel.app/"
   }
 ];
 
-const Project = ({ title, desc, link }) => {
+const Project = ({ title, desc, link, id }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
     <motion.div variants={projectVariant} className="relative">
-      <div className={overlayStyles}>
+      <div className={overlayStyles} id={`hover${id}`}>
         <p className="md:text-3xl font-playfair text-lg">{title}</p>
         <p className="mt-7 md:text-xl text-sm">{desc}</p>
-        <a target="_blank" href={link} className="hover:text-yellow transition duration-500 mt-[50px] text-lg">Visit Site </a>
+        <a target="_blank" id={`link-${id}`} href={link} className="hover:text-yellow transition duration-500 mt-[50px] text-lg">Visit Site </a>
       </div>
       <img src={`../assets/${projectTitle}.png`} alt={projectTitle} className="w-full h-full"/>
     </motion.div>
