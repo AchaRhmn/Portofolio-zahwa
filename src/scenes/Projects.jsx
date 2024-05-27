0c0f89c2be138d13fbf4aa2714809b36c73cb63c
Acha <109843061+AchaRhmn@users.noreply.github.com>
2024-05-27 09:14:03 +0800

    Update Projects.jsx
    mild change

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -106,7 +106,7 @@ const Projects = () => {
           ))}
           
           <div className="flex justify-center text-center items-center p-10 bg-blue max-w-[400px] max-h-[400px] text-3xl font-playfair font-semibold">
-            And various other simple projects.
+            And various other.
           </div>
         </motion.div>
       </div>
